8819a405cdf0d79663eac50680f37dccd4da20ab
Gustavo Rattmann <gustavo_rattmann@hotmail.com.br>
2019-12-27 16:59:43 -0300

    Aletado classe do LinkWrapper para que a Sidenav do filtro fique fullscreen

--- src/uteis/input/Button.js
+++ src/uteis/input/Button.js
@@ -8,16 +8,13 @@ export default class Button extends Component {
   render(){
     return (
       <Fragment>
-      <div>
-        <LinkWrapper to="#" data-target="filtro" className="sidenav-trigger active">
-        <div class="fixed-action-btn btn-floating btn-large blue">
-        <i class="large material-icons">search</i>
+        <div>
+          <LinkWrapper to="#" data-target="filtro" className="sidenav-trigger show-on-large active">
+            <div class="fixed-action-btn btn-floating btn-large blue">
+              <i class="large material-icons">search</i>
+            </div>
+            </LinkWrapper>
         </div>
-        </LinkWrapper>
-        <div id="filtro" className="sidenav">
-        <p>teste</p>
-        </div>
-      </div>
       </Fragment>
     )
   }
