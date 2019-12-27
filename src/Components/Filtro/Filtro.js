d2204a009fbbe6880c94c210e5dfbaf813fcaf5a
Gustavo Rattmann <gustavo_rattmann@hotmail.com.br>
2019-12-27 16:55:19 -0300

    Adicionado Sidenav com filtros

--- src/Components/Filtro/Filtro.js
+++ src/Components/Filtro/Filtro.js
@@ -1,4 +1,5 @@
 import React, {Component,Fragment} from 'react';
+import LinkWrapper from '../../uteis/LinkWrapper';
 import 'materialize-css/dist/css/materialize.min.css';
 import Pubsub from 'pubsub-js';
 
@@ -7,7 +8,6 @@ import Button from '../../uteis/input/Button';
 import Select from '../../uteis/input/Select';
 
 export default class Filtro extends Component {
-
   constructor(props){
     super(props);
     this.state = {
@@ -83,7 +83,6 @@ export default class Filtro extends Component {
       const data = this.state;
       Pubsub.publish('set-filtro',data)
     });
-
   }
 
   componentDidUpdate(prevProps, prevState){
@@ -114,7 +113,7 @@ export default class Filtro extends Component {
       <Fragment>
         <section>
           <div className="row">
-            <form onSubmit={this.pesquisa.bind(this)} className="col s12">
+            <form onSubmit={this.pesquisa.bind(this)} className="form-escuro col s12">
               <div className="row">
                 <Select name="coluna" titulo="Ordenação" valores={[{link:'ordem',nome:'Ordem'},{link:'preco-max',nome:'Preço Max'},{link:'preco-min',nome:'Preço Min'}]} selecionados={this.state.coluna}/>
                 <Select name="tipos" titulo="Tipos" valores={this.props.tipos} selecionados={this.state.tipos} multiple/>
@@ -126,9 +125,162 @@ export default class Filtro extends Component {
               </form>
             </div>
           </section>
+          <ul className="sidenav" id="filtro">
+            <li className="fecharSidenav row mb-0">
+              <LinkWrapper to="#" className="sidenav-close col s2 offset-s1"><i className="material-icons left blue-text text-darken-4">close</i></LinkWrapper>
+            </li>
+            <li className="row">
+              <form onSubmit={this.pesquisa.bind(this)} className="form-escuro col s10 offset-s1">
+                <div className="row">
+                  <div className="input-field col s12">
+                    <input type="text" name="localidade" id="localidade" className="validate"/>
+                    <label for="localidade" className="active">Localidade</label>
+                  </div>
+                  <div className="input-field col s12">
+                    <select name="tipo" id="tipo">
+                      <option disabled selected>Escolha um tipo de imóvel...</option>
+                      <option value="apartamento">Apartamento</option>
+                      <option value="casa">Casa</option>
+                      <option value="kitinete">Kitinete</option>
+                      <option value="sobrado">Sobrado</option>
+                    </select>
+                  </div>
+                  <div className="input-field col s12 condominio-check my-0 mx-auto">
+                    <p className="my-0 mx-auto">
+                      <label>
+                        <input type="checkbox" name="condominio" className="filled-in"/>
+                        <span>Condomínio</span>
+                      </label>
+                    </p>
+                  </div>
+                  <div className="input-field col s6">
+                    <i className="material-icons md-18 prefix top">attach_money</i>
+                    <input type="text" name="preco_minimo" id="preco_minimo" className="validate"/>
+                    <label for="preco_minimo" className="active">Preço mínimo</label>
+                  </div>
+                  <div className="input-field col s6">
+                    <i className="material-icons md-18 prefix top">attach_money</i>
+                    <input type="text" name="preco_maximo" id="preco_maximo" className="validate"/>
+                    <label for="preco_maximo" className="active">Preço máximo</label>
+                  </div>
+                  <div className="input-field col s6">
+                    <i className="material-icons md-18 prefix top">terrain</i>
+                    <input type="text" name="area_minima" id="area_minima" className="validate"/>
+                    <label for="area_minima" className="active">Área mínima</label>
+                  </div>
+                  <div className="input-field col s6">
+                    <i className="material-icons md-18 prefix top">terrain</i>
+                    <input type="text" name="area_maxima" id="area_maxima" className="validate"/>
+                    <label for="area_maxima" className="active">Área máxima</label>
+                  </div>
+                  <div className="input-field col s12">
+                    <p>
+                      <label>
+                        <span className="fs-14">Quartos</span>
+                      </label>
+                    </p>
+                    <p>
+                      <div className="row">
+                        <div className="col s3">
+                          <label>
+                            <input type="checkbox" name="quarto1" className="filled-in"/>
+                            <span>1</span>
+                          </label>
+                        </div>
+                        <div class="col s3">
+                          <label>
+                            <input type="checkbox" name="quarto2" className="filled-in"/>
+                            <span>2</span>
+                          </label>
+                        </div>
+                        <div className="col s3">
+                          <label>
+                            <input type="checkbox" name="quarto3" className="filled-in"/>
+                            <span>3</span>
+                          </label>
+                        </div>
+                        <div className="col s3">
+                          <label>
+                            <input type="checkbox" name="quarto4" className="filled-in"/>
+                            <span>4</span>
+                          </label>
+                        </div>
+                      </div>
+                    </p>
+                    <p>
+                      <label>
+                        <span className="fs-14">Banheiros</span>
+                      </label>
+                    </p>
+                    <p>
+                      <div className="row">
+                        <div className="col s3">
+                          <label>
+                            <input type="checkbox" name="banheiro1" className="filled-in"/>
+                            <span>1</span>
+                          </label>
+                        </div>
+                        <div className="col s3">
+                          <label>
+                            <input type="checkbox" name="banheiro2" className="filled-in"/>
+                            <span>2</span>
+                          </label>
+                        </div>
+                        <div className="col s3">
+                          <label>
+                            <input type="checkbox" name="banheiro3" className="filled-in"/>
+                            <span>3</span>
+                          </label>
+                        </div>
+                        <div className="col s3">
+                          <label>
+                            <input type="checkbox" name="banheiro4" className="filled-in"/>
+                            <span>4</span>
+                          </label>
+                        </div>
+                      </div>
+                    </p>
+                    <p>
+                      <label>
+                        <span className="fs-14">Vagas</span>
+                      </label>
+                    </p>
+                    <p>
+                      <div className="row">
+                        <div class="col s3">
+                          <label>
+                            <input type="checkbox" name="vaga1" className="filled-in"/>
+                            <span>1</span>
+                          </label>
+                        </div>
+                        <div className="col s3">
+                          <label>
+                            <input type="checkbox" name="vaga2" className="filled-in"/>
+                            <span>2</span>
+                          </label>
+                        </div>
+                        <div className="col s3">
+                          <label>
+                            <input type="checkbox" name="vaga3" className="filled-in"/>
+                            <span>3</span>
+                          </label>
+                        </div>
+                        <div className="col s3">
+                          <label>
+                            <input type="checkbox" name="vaga4" className="filled-in"/>
+                            <span>4</span>
+                          </label>
+                        </div>
+                      </div>
+                    </p>
+                  </div>
+                  <button type="reset" className="btn-small blue darken-4 col s12">Limpar</button>
+                </div>
+              </form>
+            </li>
+          </ul>
           <Button/>
       </Fragment>
-
     )
   }
 }
